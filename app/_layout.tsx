9e5e06727543453d2d569f0c155f44720ff41b1d
Suman Jyoti Phukan <mystogan3780@gmail.com>
2025-03-21 17:30:45 +0530

    Initial Commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Drawer } from "expo-router/drawer";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer screenOptions={{}}>
+        <Drawer.Screen
+          name="index"
+          options={{
+            drawerLabel: "Home",
+            title: "Dashboard",
+          }}
+        />
+        <Drawer.Screen
+          name="policy"
+          options={{
+            drawerLabel: "Policy",
+            title: "Policy",
+          }}
+        />
+        <Drawer.Screen
+          name="wallet"
+          options={{
+            drawerLabel: "Wallet",
+            title: "Wallet",
+          }}
+        />
+        <Drawer.Screen
+          name="profile"
+          options={{
+            drawerLabel: "Profile",
+            title: "Profile",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
---
+++ app/wallet.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import React from "react";
+
+export default function Wallet() {
+  return (
+    <View>
+      <Text>Wallet</Text>
+    </View>
+  );
+}
